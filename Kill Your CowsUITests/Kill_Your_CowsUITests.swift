9012aa92f8e1895618186c7edf6b859b4c24090b
EBUJOLD <emilyebujold@gmail.com>
2018-06-13 15:05:20 -0700

    I Hate terminal

---
+++ Kill Your CowsUITests/Kill_Your_CowsUITests.swift
@@ -0,0 +1,36 @@
+//
+//  Kill_Your_CowsUITests.swift
+//  Kill Your CowsUITests
+//
+//  Created by Bujold, Emily E (382E-Affiliate) on 6/13/18.
+//  Copyright © 2018 Emily Bujold. All rights reserved.
+//
+
+import XCTest
+
+class Kill_Your_CowsUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# kill_your_cows
